53f8c8f058852b36ad3712a6265cb9564399ec57
boosha969 <104218836+boosha969@users.noreply.github.com>
2023-06-05 02:18:18 -0700

    Add files via upload

---
+++ products.js
@@ -0,0 +1,50 @@
+export const productsData = [
+  {
+    id: 1,
+    title: "تخت خواب دونفره",
+    price: 10.99,
+    imageUrl: "./images/product-1.jpeg",
+  },
+  {
+    id: 2,
+    title: "تخت خواب دو نفره مخصوص ماه عسل",
+    price: 14.99,
+    imageUrl: "./images/product-2.jpeg",
+  },
+  {
+    id: 3,
+    title: "تخت اسپرت و روشن دونفره",
+    price: 7.99,
+    imageUrl: "./images/product-3.jpeg",
+  },
+  {
+    id: 4,
+    title: "تخت دانشجویی",
+    price: 11.99,
+    imageUrl: "./images/product-4.jpeg",
+  },
+  {
+    id: 5,
+    title: "اشپز خانه مدل لاکچری",
+    price: 4.99,
+    imageUrl: "./images/product-5.jpeg",
+  },
+  {
+    id: 6,
+    title: "تخت لش کن تک نفره",
+    price: 23.99,
+    imageUrl: "./images/product-6.jpeg",
+  },
+  {
+    id: 7,
+    title: "مبل طرح خاکستری",
+    price: 42.99,
+    imageUrl: "./images/product-7.jpeg",
+  },
+  {
+    id: 8,
+    title: "گل اپارتمانی ",
+    price: 24.99,
+    imageUrl: "./images/product-8.jpeg",
+  },
+];
